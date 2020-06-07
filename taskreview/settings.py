4cf57d70a23fef9748ee2416489ed2753cd2b10e
Suvajit Sarkar <suvajit.sarkar@eternalrobotics.com>
2020-06-07 19:36:32 +0530

    Remove / from allowed host

--- taskreview/settings.py
+++ taskreview/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '=nh*!@kfz%e$0=$xl-ks-i44n$j0%vzx_yoiz*trk^9%ucq02_'
 DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['quiet-basin-15744.herokuapp.com/']
+ALLOWED_HOSTS = ['quiet-basin-15744.herokuapp.com']
 #ALLOWED_HOSTS = []
 
 
